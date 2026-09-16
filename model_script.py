fc0963319b073ff79593f171864ab6767f8d25c5
Schlutzkrapfen <david.messner@protonmail.com>
2026-09-16 11:55:52 +0200

    added some prints for easier debbging

--- model_script.py
+++ model_script.py
@@ -76,7 +76,9 @@ class YOLOTopKBackend(LabelStudioMLBase):
                 "result": result_items,
                 "score": sum(item["score"] for item in result_items) / len(result_items) if result_items else 0,
             })
+        print(predictions)
         if not predictions:
+            print("something went wrong not sure where")
             return None
 
         return {"predictions": predictions }
